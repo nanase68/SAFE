a98d72270357ffd0df752213f13096a81981976f
Yusuke SHIMA <mashi4729@gmail.com>
2014-12-08 18:33:38 +0900

    InputActor仮

---
+++ actor_test/InputActor.cpp
@@ -0,0 +1,18 @@
+/*
+ * InputActor.cpp
+ *
+ *  Created on: 2014/12/08
+ *      Author: Ys
+ */
+
+#include <InputActor.h>
+
+InputActor::InputActor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+InputActor::~InputActor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ actor_test/InputActor.h
@@ -0,0 +1,19 @@
+/*
+ * InputActor.h
+ *
+ *  Created on: 2014/12/08
+ *      Author: Ys
+ */
+
+#ifndef INPUTACTOR_H_
+#define INPUTACTOR_H_
+
+#include <Actor.h>
+
+class InputActor: public Actor {
+public:
+	InputActor();
+	virtual ~InputActor();
+};
+
+#endif /* INPUTACTOR_H_ */
