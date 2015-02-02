95026e072618b3836ff571baa24515344ad11701
Yusuke SHIMA <mashi4729@gmail.com>
2015-02-02 13:02:17 +0900

    InterruptCompositeを作成

--- actor_test/TickerComposite.cpp
+++ actor_test/TickerComposite.cpp
@@ -5,16 +5,19 @@
  *      Author: Ys
  */
 
-#include "TickerComposite.h"
 #include "mbed.h"
 #include "Message.h"
 #include "SystemActor.h"
+#include "TickerComposite.h"
 
 // MEMO: ハンドラモードでprintfやdelete, newをしてはいけない！
+/*
+ *
+ */
 void TickerComposite::autoSend(){
 	sysActor.sendToPriorityQueue(TickerComposite::destination, TickerComposite::message);
 }
-TickerComposite::TickerComposite(Actor *dest, Message *msg, float periodicTime): FlipperComposite(dest, msg){
+TickerComposite::TickerComposite(Actor *dest, Message *msg, float periodicTime): BaseComposite(dest, msg){
 	TickerComposite::ticker = new Ticker();
 	TickerComposite::ticker->attach(this, &TickerComposite::autoSend, periodicTime);
 }
@@ -23,11 +26,14 @@ TickerComposite::~TickerComposite(){
 	delete ticker;
 }
 
+/*
+ *
+ */
 void TimeoutComposite::autoSend(){
 	sysActor.sendToPriorityQueue(TimeoutComposite::destination, TimeoutComposite::message);
 	sysActor.sendTo(&sysActor, sysActor.getMsgDeleteToc());
 }
-TimeoutComposite::TimeoutComposite(Actor *dest, Message *msg, float waitTime): FlipperComposite(dest, msg){
+TimeoutComposite::TimeoutComposite(Actor *dest, Message *msg, float waitTime): BaseComposite(dest, msg){
 	TimeoutComposite::timeout = new Timeout();
 	TimeoutComposite::timeout->attach(this, &TimeoutComposite::autoSend, waitTime);
 }
@@ -35,3 +41,17 @@ TimeoutComposite::TimeoutComposite(Actor *dest, Message *msg, float waitTime): F
 TimeoutComposite::~TimeoutComposite(){
 	delete timeout;
 }
+
+/*
+ *
+ */
+void InterruptComposite::autoSend(){
+	sysActor.sendToPriorityQueue(InterruptComposite::destination, InterruptComposite::message);
+}
+void InterruptComposite::rise(){
+	InterruptComposite::interrputIn.rise(this, &InterruptComposite::autoSend);
+}
+void InterruptComposite::fall(){
+	InterruptComposite::interrputIn.fall(this, &InterruptComposite::autoSend);
+}
+
